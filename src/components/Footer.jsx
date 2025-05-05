7bc828925428c5ae50b71c74d2cbada5f0dc148e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 18:57:54 +0100

    add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer bg-dark text-light">
+            <div className="text-center">
+                <span>© {new Date().getFullYear()} Developed by RaxRot</span>
+            </div>
+        </footer>
+    );
+}
