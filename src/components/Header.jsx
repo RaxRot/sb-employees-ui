48c8cbe73d5e16b9b60a42a4081b83dee42dba29
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 18:57:47 +0100

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Employee Management System</span>
+                </div>
+            </nav>
+        </header>
+    );
+}
