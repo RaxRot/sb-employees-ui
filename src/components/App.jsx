eb88fa59ca57445b05c9a66e2e095d839c718554
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 18:58:27 +0100

    add Header Footer and ListEmployee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,15 @@
 import './App.css'
 import HelloWorld from "./HelloWorld.jsx";
 import ListEmployeeComponent from "./ListEmployeeComponent.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
-  return <ListEmployeeComponent/>
+  return <>
+    <Header/>
+    <ListEmployeeComponent/>
+    <Footer/>
+         </>
 }
 
 export default App
