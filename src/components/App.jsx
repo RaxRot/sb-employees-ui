3091a43d597a0c077f92da457e42af2e097ed811
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 19:51:37 +0100

    config route for ListEmployee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,18 @@ import ListEmployeeComponent from "./ListEmployeeComponent.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 function App() {
   return <>
+      <BrowserRouter>
     <Header/>
-    <ListEmployeeComponent/>
+          <Routes>
+              <Route path='/' element={<ListEmployeeComponent/>}></Route>
+              <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+          </Routes>
     <Footer/>
+      </BrowserRouter>
          </>
 }
 
