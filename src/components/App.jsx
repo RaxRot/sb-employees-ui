567ec4ac03247029b95ee11642f1a2bf8f6a92b6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 18:44:20 +0100

    prepare basic

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+import HelloWorld from "./HelloWorld.jsx";
+import ListEmployeeComponent from "./ListEmployeeComponent.jsx";
+
+function App() {
+  return <ListEmployeeComponent/>
+}
+
+export default App
