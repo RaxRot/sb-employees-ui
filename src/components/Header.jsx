4813f0460767fb9678f9fe1907839524709abfd6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-06 14:58:34 +0100

    add links to empl and departments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,38 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Header() {
     return (
         <header>
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Employee Management System</span>
+                    <NavLink to="/" className="navbar-brand">
+                        Employee Management System
+                    </NavLink>
+
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/employees"
+                                    className={({ isActive }) =>
+                                        'nav-link' + (isActive ? ' active' : '')
+                                    }
+                                >
+                                    Employees
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/departments"
+                                    className={({ isActive }) =>
+                                        'nav-link' + (isActive ? ' active' : '')
+                                    }
+                                >
+                                    Departments
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
